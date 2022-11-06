4a0a85bd524d9e386e81c9232e6c7051d614fe13
Volodymyr <zmiyov88@gmail.com>
2022-11-06 12:37:37 +0200

    Binded the values to user interface

--- MVVM_ GoodWeather/Controllers/WeatherDetailsViewController.swift
+++ MVVM_ GoodWeather/Controllers/WeatherDetailsViewController.swift
@@ -21,9 +21,21 @@ class WeatherDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let weatherVM = weatherViewModel {
-            self.cityNameLabel.text = weatherVM.name.value
-            self.currentTemperatureLabel.text = weatherVM.currentTemperature.temperature.value.formatAsDegree
+        setupViewModelBindings()
+        
+    }
+    
+    private func setupViewModelBindings() {
+        
+        if let weatherVM = self.weatherViewModel {
+            
+            weatherVM.name.bind { self.cityNameLabel.text = $0 }
+            weatherVM.currentTemperature.temperature.bind { self.currentTemperatureLabel.text = $0.formatAsDegree }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            
+            self.weatherViewModel?.name.value = "Boston"
         }
     }
 }
